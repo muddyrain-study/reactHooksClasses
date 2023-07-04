950fda53b9b1429aeabdb6212fb1b45b028f4301
“muddyrain” <qiu3291002845@gmail.com>
2023-07-04 09:20:28 +0800

    feat: useSafeState

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import { useState } from 'react';
-import { useMount, useUnmount } from './hooks';
+import { useUpdate } from '@/hooks';
 import { Button, message } from 'antd';
 
-const Child = () => {
-  useMount(() => {
-    message.info('首次渲染');
-  });
-
-  useUnmount(() => {
-    message.info('组件已卸载');
-  });
-
-  return <div>大家好，我是小杜杜，一起玩转Hooks吧！</div>;
-};
-
 const Index = () => {
-  const [flag, setFlag] = useState<boolean>(false);
+  const update = useUpdate();
 
   return (
     <div>
-      <Button type='primary' onClick={() => setFlag(v => !v)}>
-        切换 {flag ? 'unmount' : 'mount'}
+      <div>时间：{Date.now()}</div>
+      <Button
+        type='primary'
+        onClick={() => {
+          update();
+        }}
+      >
+        更新
       </Button>
-      {flag && <Child />}
     </div>
   );
 };
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 export { default as useMount } from './useMount';
 export { default as useUnmount } from './useUnmount';
 export { default as useLatest } from './useLatest';
+export { default as useUnmountedRef } from './useUnmountedRef';
+export { default as useSafeState } from './useSafeState';
+export { default as useUpdate } from './useUpdate';
